2b938b169e971e302844a68851af43c39701116e
Eugene Pankov <john.pankov@gmail.com>
2015-01-09 18:04:07 +0300

    initial version

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='ajenti-dev-multitool',
+    version='0.2',
+    install_requires=[],
+    description='-',
+    author='Eugeny Pankov',
+    author_email='e@ajenti.org',
+    url='http://ajenti.org/',
+    scripts=['ajenti-dev-multitool'],
+)
