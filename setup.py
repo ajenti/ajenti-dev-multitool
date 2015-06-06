d7f53e06df0961f32b7f72a77d1940c5d0e490c4
Eugene Pankov <john.pankov@gmail.com>
2015-06-06 14:31:23 +0300

    dist cleanup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.0.2',
+    version='1.0.3',
     install_requires=[
         'coloredlogs',
         'pyyaml',
