8c9d802c49cb9f2f3ee97253ab2ba60fddb5a117
Eugene Pankov <john.pankov@gmail.com>
2015-08-28 20:56:11 +0300

    add READMEs to setuptools pkgs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.0.4',
+    version='1.0.5',
     install_requires=[
         'coloredlogs',
         'pyyaml',
