93c1d8b59bc193b26af880ca802e5cbc700f5d0a
Eugene Pankov <john.pankov@gmail.com>
2015-08-28 20:57:29 +0300

    fixed manifest

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.0.5',
+    version='1.0.6',
     install_requires=[
         'coloredlogs',
         'pyyaml',
