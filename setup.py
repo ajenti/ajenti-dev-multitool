35e2376fb18572b97e564bcab2e601e53088bd65
Eugene Pankov <john.pankov@gmail.com>
2015-09-18 17:32:04 +0200

    .

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.0.9',
+    version='1.0.10',
     install_requires=[
         'coloredlogs',
         'pyyaml',
