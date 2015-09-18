bd613b2f67b2ff3aa6d99b142c5c90382cbd5cea
Eugene Pankov <john.pankov@gmail.com>
2015-09-18 17:30:38 +0200

    xgettext encoding

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.0.8',
+    version='1.0.9',
     install_requires=[
         'coloredlogs',
         'pyyaml',
