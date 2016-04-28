9bd4df31525bc651997d8f6d1ec057885b545685
Eugene Pankov <john.pankov@gmail.com>
2016-04-28 16:10:03 +0200

    don't use dashes in plugin names

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.0.15',
+    version='1.0.16',
     install_requires=[
         'coloredlogs',
         'pyyaml',
