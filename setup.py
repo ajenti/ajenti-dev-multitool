1b2f584b915e24447c4d6ee07476ded6d57d5248
Eugene Pankov <john.pankov@gmail.com>
2016-10-07 21:11:01 +0200

    babel es6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.0.16',
+    version='1.0.17',
     install_requires=[
         'coloredlogs',
         'pyyaml',
