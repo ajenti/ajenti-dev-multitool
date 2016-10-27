a9c6bbc1bd29e75ea04b55b2dcd15837408c911b
Eugene Pankov <john.pankov@gmail.com>
2016-10-27 17:30:45 +0200

    handle build error exitcodes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.0.18',
+    version='1.0.20',
     install_requires=[
         'coloredlogs',
         'pyyaml',
