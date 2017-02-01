cf5f6c583da392d633a775aeb1b01f2070ff7e97
Eugene Pankov <john.pankov@gmail.com>
2017-02-01 17:08:24 +0100

    use external babel helpers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.1.0',
+    version='1.1.1',
     install_requires=[
         'coloredlogs',
         'pyyaml',
