ec30f5db3401f7ac8fa1f5791fc69e33820a593f
Eugene Pankov <john.pankov@gmail.com>
2018-11-28 21:32:48 +0100

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='ajenti-dev-multitool',
-    version='1.1.6',
+    version='1.1.7',
     install_requires=[
         'coloredlogs',
         'pyyaml',
